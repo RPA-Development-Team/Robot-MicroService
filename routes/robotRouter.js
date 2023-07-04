d22f5ca4af5d1d07afdb1d6521e203f5d9f4ba40
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-07-04 17:11:07 +0200

    Added robot api routes

--- routes/robotRouter.js
+++ routes/robotRouter.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const robotController = require('../controllers/robotController');
+const robotApiHandler = require('../controllers/robotApiHandler');
+const robotApiHandler = require('../endpoints/handlers/robotHandler')
 
 module.exports = () => {
     router.post('/package', robotController.receivePackage);
     router.get('/:name', robotController.handleRobotLogs);
+
+    router.get('/api/get/id/:robotID', robotApiHandler.getRobotByID)
+    router.get('/api/get/address/:robotAdress', robotApiHandler.getRobotByAddress)
+    router.get('/api/get/user/:userID', robotApiHandler.getUserRobots)
+    router.get('/api/get/connected/user/:userID', robotApiHandler.getUserConnectedRobots)
+    router.get('/api/get/connected', robotApiHandler.getAllConnectedRobots)
+    router.get('/api/get/', robotApiHandler.getAllRobots)
 
     return router
 }
