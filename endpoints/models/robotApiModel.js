0dc9db90487c44c466b969b87672289164247c49
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-07-04 17:12:27 +0200

    Added robot api model

---
+++ endpoints/models/robotApiModel.js
@@ -0,0 +1,103 @@
+class RobotApiModel {
+    constructor(prisma) {
+        this.prisma = prisma
+    }
+
+     async GetAllRobots() {
+        try {
+            const robots = await this.prisma.Robot.findMany()
+            if (!robots) {
+                console.log(`No Robots yet found`)
+                return null
+            }
+            return robots
+        } catch (err) {
+            console.log(`[Model-Handling-Error]: Failed to get all robots`)
+            console.log(`Error: ${err.message}`)
+            throw new Error(`Failed to get all robots`)
+        }
+    }
+
+     async GetRobotByID(robotID) {
+        try {
+            const robot = await this.prisma.Robot.findUnique({
+                where: { id: robotID }
+            })
+            if (!robot) {
+                console.log(`No Such Robot found`)
+                return null
+            }
+            return robot
+        } catch (err) {
+            console.log(`Error: ${err.message}`)
+            throw new Error(`[Model-Handling-Error]: Failed to get robot entity with robot-id`)
+        }
+    }
+
+    async GetRobotByAddress(robotAddress) {
+        try {
+          const robot = await this.prisma.Robot.findFirst({
+            where: { robotAddress: robotAddress }
+          });
+          if (!robot) {
+            console.log(`No such robot found`);
+            return null;
+          }
+          return robot;
+        } catch (err) {
+          console.log(`Error: ${err.message}`);
+          throw new Error(`[Model-Handling-Error]: Failed to get robot entity with robot-address`);
+        }
+      }
+      
+
+     async GetUserRobots(userID) {
+        try {
+            const robots = await this.prisma.Robot.findMany({
+                where: { userID: userID }
+            })
+            if (!robots) {
+                console.log(`No Robots belongs to this user`)
+                return null
+            }
+            return robots
+        } catch (err) {
+            console.log(`Error: ${err.message}`)
+            throw new Error(`[Model-Handling-Error]: Failed to get robot entities with user-id`)
+        }
+    }
+
+     async GetAllConnectedRobots() {
+        try {
+            const robots = await this.prisma.Robot.findMany({
+                where: { connected: true }
+            })
+            if (!robots) {
+                console.log(`No Current Active Robots`)
+                return null
+            }
+            return robots
+        } catch (err) {
+            console.log(`Error: ${err.message}`)
+            throw new Error(`[Model-Handling-Error]: Failed to get connected robot entities`)
+        }
+    }
+
+     async GetUserConnectedRobots(userID) {
+        try {
+            const robot = await this.prisma.Robot.findMany({
+                where: { userID: userID, connected: true }
+            })
+            if (!robot) {
+                console.log(`No Active Robots currently belongs to this user`)
+                return null
+            }
+            return robot
+        } catch (err) {
+            console.log(`Error: ${err.message}`)
+            throw new Error(`[Model-Handling-Error]: Failed to get connected robot entities with user-id`)
+        }
+    }
+}
+
+module.exports = RobotApiModel
